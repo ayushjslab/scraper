4866b215b36cbeb869cc2e5ec3c5edb10eea664a
Ayush Saini <ayushsaini1910@gmail.com>
2026-02-04 12:50:53 +0530

    fix bug

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { scrapeWebsite } from "@/lib/scraper";
-
+export const runtime = "nodejs";
 export async function GET(req: Request) {
 
     try {
